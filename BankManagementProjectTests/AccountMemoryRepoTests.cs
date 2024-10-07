c7ca4a5580201c32a4b575ffed1a63223e7dec60
anugrahkrishnan <64000608+anugrahkrishnan@users.noreply.github.com>
2024-10-07 23:54:27 +0530

    Add files via upload

---
+++ BankManagementProjectTests/AccountMemoryRepoTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankManagementProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace BankManagementProject.Tests
+{
+    [TestClass()]
+    public class AccountMemoryRepoTests
+    {
+        private AccountMemoryRepo _repo;
+        [TestInitialize]
+        public void initialize()
+        {
+            _repo = AccountMemoryRepo.Instance;
+        }
+        [TestMethod()]
+        public void CreateTest()
+        {
+            var account = new AccountModel()
+            {
+                AccNo = 49,
+                Name = "Ammu",
+                Balance = 0,
+                AccType = "current",
+                Email = "ammu@gmail.com",
+                PhNo = "5236526526",
+                Address = "address",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            };
+            _repo.Create(account);
+
+            Assert.IsTrue(_repo.ReadAllAccount().Any(ac => ac.AccNo == 49));
+        }
+
+        [TestMethod()]
+        public void UpdateAccountTest()
+        {
+            AccountModel account = new AccountModel
+            {
+                AccNo = 1234,
+                Name = "Anamika",
+                Balance = 0,
+                AccType = "savings",
+                Email = "anamika@gmail.com",
+                PhNo = "789456123",
+                Address = "xxx street",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            };
+
+            account.Address = "New Address";
+            _repo.UpdateAccount(account);
+            Assert.AreEqual("New Address", _repo.ReadAllAccount().First(a => a.AccNo == 1234).Address);
+        }
+
+        [TestMethod()]
+        public void DepositTest()
+        {
+            AccountModel account = new AccountModel
+            {
+                AccNo = 1234,
+                Name = "Anamika",
+                Balance = 0,
+                AccType = "savings",
+                Email = "anamika@gmail.com",
+                PhNo = "789456123",
+                Address = "Address",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            };
+
+            _repo.Deposit(account.AccNo, 2000);
+            Assert.AreEqual(2000, _repo.ReadAllAccount().First(ac => ac.AccNo == account.AccNo).Balance);
+        }
+
+        [TestMethod()]
+        public void WithdrawTest()
+        {
+            var account = new AccountModel()
+            {
+                AccNo = 4567,
+                Name = "Paru",
+                Balance = 0,
+                AccType = "current",
+                Email = "paru@gmail.com",
+                PhNo = "4561233",
+                Address = "yyy street",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            };
+
+            _repo.Deposit(account.AccNo, 500);
+            _repo.Withdraw(account.AccNo, 200);
+            Assert.AreEqual(300, _repo.ReadAllAccount().First(ac => ac.AccNo == account.AccNo).Balance);
+        }
+
+        [TestMethod()]
+        public void ReadAllAccountTest()
+        {
+            _repo = AccountMemoryRepo.Instance;
+            var accounts = _repo.ReadAllAccount();
+
+            Assert.AreEqual(2, accounts.Count);
+            Assert.IsTrue(accounts.Any(a => a.AccNo == 1234));
+            Assert.IsTrue(accounts.Any(a => a.AccNo == 4567));
+        }
+    }
+}
---
+++ BankManagementProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BankManagementProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("QuEST-Global")]
+[assembly: AssemblyProduct("BankManagementProjectTests")]
+[assembly: AssemblyCopyright("Copyright © QuEST-Global 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fcff2283-156d-4e9f-9122-6d045cab1868")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
